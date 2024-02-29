74332edb6d62da61f5882877958a528425806ec7
Luca-Brabender <luca@brabenders.de>
2024-02-29 20:31:31 +0100

    added GeometryReader to ShoppingCart.swift

--- SweetyUI/ShoppingCartView.swift
+++ SweetyUI/ShoppingCartView.swift
@@ -12,21 +12,29 @@ struct ShoppingCartView: View {
     
     var body: some View {
         
-        VStack(alignment: .leading){
-            Text("Shopping Cart")
-                .font(.system(size: 30))
-                .padding(.bottom, 80)
-            Text("No items")
-                .font(.headline)
-            Text("Login to view your shopping cart")
-                .padding(.bottom, 20)
-                .foregroundColor(.gray)
-            
-            SimpleLoginButtonView()
-                .padding(.bottom, 450)
-            
+        GeometryReader{ geometry in
+            VStack(alignment: .leading){
+                Text("Shopping Cart")
+                    .font(.system(size: 30))
+                    
+                
+                Text("No items")
+                    .font(.headline)
+                    .position(x: geometry.size.width*0.17, y:geometry.size.height*0.2)
+                Text("Login to view your shopping cart")
+                    .foregroundColor(.gray)
+                    .position(x:geometry.size.width*0.4, y:geometry.size.height*0.0)
+                
+                SimpleLoginButtonView()
+                    .position(
+                        x: geometry.size.width*0.2,
+                        y:geometry.size.height*(-0.23)
+                    )
+                    
+                
+            }.padding(23)
         }
-        .padding(.trailing, 85)
+        
     }
 }
 
